585393db64dd6e9794fad12a611a834993c4cd90
jisoo0507 <jisoo0507@naver.com>
2024-12-01 13:39:42 +0900

    feat: test 폴더 추가

---
+++ src/test/java/com/jisoo/solerun/SoleRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisoo.solerun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoleRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
