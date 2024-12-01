39ada39a9112ab4d19711660433ee892cd88b477
jisoo0507 <jisoo0507@naver.com>
2024-12-01 13:27:23 +0900

    feat: project 실행 클래스 추가

---
+++ src/main/java/com/jisoo/solerun/SoleRunApplication.java
@@ -0,0 +1,13 @@
+package com.jisoo.solerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoleRunApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(SoleRunApplication.class, args);
+    }
+
+}
