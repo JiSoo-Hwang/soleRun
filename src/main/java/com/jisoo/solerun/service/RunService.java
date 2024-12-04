378ed7968c7e9b14b1604c65e4ac0862fd6fa386
jisoo0507 <jisoo0507@naver.com>
2024-12-04 23:09:48 +0900

    feat/refractor: 게시글 수정/삭제 service 단 메서드 생성, final 필드인 RunRepository 필드에 대한 생성자 자동으로 생성되게끔 해주는 @RequiredArgsConstructor 추가, 기존 생성자 코드는 삭제

--- src/main/java/com/jisoo/solerun/service/RunService.java
+++ src/main/java/com/jisoo/solerun/service/RunService.java
@@ -1,5 +1,6 @@
 package com.jisoo.solerun.service;
 
+import com.jisoo.solerun.dto.RunUpdateDto;
 import com.jisoo.solerun.entity.Run;
 import java.util.List;
 
@@ -7,4 +8,6 @@ public interface RunService {
     Run saveRun(Run run);
     List<Run> selectRun();
     Run getRunById(Long id);
+    boolean updateRun(RunUpdateDto dto);
+    boolean deleteRun(Long id);
 }
--- src/main/java/com/jisoo/solerun/service/RunServiceImpl.java
+++ src/main/java/com/jisoo/solerun/service/RunServiceImpl.java
@@ -1,18 +1,20 @@
 package com.jisoo.solerun.service;
 
+import com.jisoo.solerun.dto.RunUpdateDto;
 import com.jisoo.solerun.entity.Run;
 import com.jisoo.solerun.repository.RunRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class RunServiceImpl implements RunService{
     private final RunRepository runRepository;
 
-    public RunServiceImpl(RunRepository runRepository){
-        this.runRepository=runRepository;
-    }
     //게시글 추가
     @Override
     public Run saveRun(Run run) {
@@ -29,4 +31,29 @@ public class RunServiceImpl implements RunService{
         return runRepository.findById(id)
                 .orElseThrow(()->new RuntimeException("해당 게시글을 찾을 수 없습니다."));
     }
+
+    @Override
+    @Transactional
+    public boolean updateRun(RunUpdateDto dto) {
+        Optional<Run> optionalRun = runRepository.findById(dto.getId());
+        if(optionalRun.isPresent()){
+            Run run = optionalRun.get();
+            run.setRunTitle(dto.getTitle());
+            run.setRunStory(dto.getStory());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteRun(Long id) {
+        Optional<Run> optionalRun = runRepository.findById(id);
+        if(optionalRun.isPresent()){
+            runRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+
 }
